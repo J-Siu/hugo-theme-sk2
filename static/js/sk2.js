ee079581810a0bc8241b995be38b0ea290c2688b
John Siu (imacpro) <john.sd.siu@gmail.com>
2020-07-03 00:19:10 -0400

    Clean up

--- static/js/sk2.js
+++ static/js/sk2.js
@@ -1,8 +1,10 @@
+// toc
 var c = document.getElementsByClassName("collapsible");
 for (var i = 0; i < c.length; i++) {
+	c[i].display = "none"
 	c[i].addEventListener("click", function () {
 		this.classList.toggle(false);
-		var el = this.nextElementSibling;
-		el.style.display === "block" ? el.style.display = "none" : el.style.display = "block";
+		var e = this.nextElementSibling;
+		e.style.display === "block" ? e.style.display = "none" : e.style.display = "block";
 	},false)
 }
